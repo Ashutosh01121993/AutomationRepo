90b0e850307fc9a980c805aad6b5ed374997b61f
AshutoshTripathi01121993 <ashutria8@gmail.com>
2019-09-30 11:00:52 +0530

    Message Display Change
    Message Display Change

--- Basiccorejava/src/DatatypesAndVariables/FirstJavaClass.java
+++ Basiccorejava/src/DatatypesAndVariables/FirstJavaClass.java
@@ -3,7 +3,10 @@ package DatatypesAndVariables;
 public class FirstJavaClass {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Selenium!!");
+		System.out.println("India Won 2011 Cricket World Cup!");
+		System.out.println("User name password Should be Correct");
+		
+		
 
 	}
 
